7c1d71a700ab2464e361e66b788c54f9657a84b0
Renegade <bhavinpbhanushali@gmail.com>
2021-12-11 03:41:44 +0530

    Added Time Elapsed & Duration text

--- lib/components/AudioSlider.dart
+++ lib/components/AudioSlider.dart
@@ -18,6 +18,11 @@ class _AudioSliderState extends State<AudioSlider> {
   Duration? duration;
   Duration? position;
 
+  get durationText =>
+      duration != null ? duration.toString().split('.').first.substring(2) : '';
+  get positionText =>
+      position != null ? position.toString().split('.').first.substring(2) : '';
+
   @override
   Widget build(BuildContext context) {
     audioController.audioPlayer.onPlayerStateChanged.listen((PlayerState s) {
@@ -39,14 +44,45 @@ class _AudioSliderState extends State<AudioSlider> {
       });
     });
 
-    return Slider(
-      value: position == null ? 0.0 : position!.inSeconds.toDouble(),
-      min: 0.0,
-      max: duration == null ? 0.0 : duration!.inSeconds.toDouble(),
-      onChanged: (v) =>
-          audioController.audioPlayer.seek(Duration(seconds: v.toInt())),
-      activeColor: Colors.lightGreenAccent,
-      inactiveColor: Colors.white,
+    return Column(
+      children: [
+        Slider(
+          value: position == null ? 0.0 : position!.inSeconds.toDouble(),
+          min: 0.0,
+          max: duration == null ? 0.0 : duration!.inSeconds.toDouble(),
+          onChanged: (v) =>
+              audioController.audioPlayer.seek(Duration(seconds: v.toInt())),
+          activeColor: Colors.lightGreenAccent,
+          inactiveColor: Colors.white,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              margin: const EdgeInsets.only(left: 22),
+              child: Text(
+                position != null
+                    ? "${positionText ?? ''} / ${durationText ?? ''}"
+                    : "00:00",
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 15.0,
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(right: 22),
+              child: Text(
+                duration == null ? "00:00" : durationText,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 15.0,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
